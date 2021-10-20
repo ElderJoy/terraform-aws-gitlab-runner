f8602bd1f343cf3e435258fd4383d78d23a1a41b
ElderJoy <blin@bigmir.net>
2021-10-21 01:04:28 +0300

    adjust runner instance type

--- variables.tf
+++ variables.tf
@@ -50,7 +50,7 @@ variable "metrics_autoscaling" {
 variable "instance_type" {
   description = "Instance type used for the GitLab runner."
   type        = string
-  default     = "t3.micro"
+  default     = "t2.micro"
 }
 
 variable "runner_instance_ebs_optimized" {
